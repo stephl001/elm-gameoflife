e1faa40702c3b510f0ea3d976026c8a42eccfac3
Stephane Larocque <stephl001@gmail.com>
2018-09-21 20:48:33 -0400

    Add tests for cell toggling

--- tests/GenerationTests.elm
+++ tests/GenerationTests.elm
@@ -9,14 +9,28 @@ import TestUtilities exposing (..)
 
 suite : Test
 suite =
+    let
+        ( genHeight, genWidth ) =
+            ( 5, 5 )
+
+        genFuzzer =
+            intRange 0 (2 ^ genHeight ^ genWidth - 1)
+
+        genCreator =
+            genFromInt genHeight genWidth
+
+        toggleAllCells : Gen -> Gen
+        toggleAllCells gen =
+            List.range 0 (genHeight * genWidth - 1)
+                |> List.foldl (\i -> Generation.toggleCellState (i // genWidth) (i % genWidth)) gen
+    in
     describe "The Generation module"
         [ describe "Create Generation"
-            -- Nest as many descriptions as you like.
-            [ fuzz (intRange 0 (2 ^ 5 ^ 5 - 1)) "Ensure we can construct and deconstruct a random generation" <|
+            [ fuzz genFuzzer "Ensure we can construct and deconstruct a random generation" <|
                 \someInt ->
                     let
                         randomGen =
-                            genFromInt 5 5 someInt
+                            genCreator someInt
 
                         fromToGen =
                             Generation.toList >> Generation.fromList
@@ -25,5 +39,38 @@ suite =
                             randomGen |> fromToGen
                     in
                     Expect.equal randomGen constructedGen
+            ]
+        , describe "Generation Dimensions"
+            [ fuzz2 (intRange 1 20) (intRange 1 20) "Ensure we can valid dimensions for a generation" <|
+                \someHeight someWidth ->
+                    let
+                        randomGen =
+                            Generation.repeat someHeight someWidth Dead
+
+                        dim =
+                            randomGen |> Generation.getDimensions
+                    in
+                    Expect.equal dim ( someHeight, someWidth )
+            ]
+        , describe "Generation Cell Toggling - All Cells Twice"
+            [ fuzz genFuzzer "Toggling all cells of a random generation twice should yield the original generation" <|
+                \someInt ->
+                    let
+                        randomGen =
+                            genCreator someInt
+
+                        toggleAllCellsTwice =
+                            toggleAllCells >> toggleAllCells
+                    in
+                    Expect.equal randomGen (randomGen |> toggleAllCellsTwice)
+            ]
+        , describe "Generation Cell Toggling - All Cells Once"
+            [ fuzz genFuzzer "Toggling all cells of a random generation once should yield a different generation" <|
+                \someInt ->
+                    let
+                        randomGen =
+                            genCreator someInt
+                    in
+                    Expect.notEqual randomGen (randomGen |> toggleAllCells)
             ]
         ]
